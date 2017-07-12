64b94636e32963a61d9720f4634fb4357e59cca9
Carlos Beleño <cbeleno@redsis.com>
2017-07-12 16:02:11 -0500

    Add CHANGELOG

--- metadata.rb
+++ metadata.rb
@@ -4,29 +4,7 @@ maintainer_email 'cbeleno@redsis.com'
 license 'all_rights'
 description 'Installs/Configures deploy_tomcat_app'
 version '2.0.1'
-long_description <<-DSCPT
-#### Cookbook to deploy Tomcat Applications in a big number of nodes
-
-##### Changelog v2.0.1 11/05/2017:
-
-- Add resource `ruby_block \"Verify if #{$app_name} is deployed\"` to `deploy.rb` recipe.
-
-##### Changelog v2.0.0 09/05/2017:
-
-- Add `depends` and include default recipe of `tomcat_manager` cookbook.
-
-- Delete default attributes related to `Tomcat Manager`. These are defined by `tomcat_manager` cookbook.
-
-##### Changelog v1.1.0 28/04/2017:
-
-- New recipe `download_sw_64bits.rb` to downloaded Tomcat and Java of 64 bits if deployment fails.
-
-- Fix `deploy_app` method of `TomcatManager` module, it failed using `simple_email` method of `Tool` module.
-
-- Add resource `ruby_block 'Wait for tomcat to start'` to `deploy.rb` recipe.
-
-DSCPT
-
+long_description 'Cookbook to deploy Tomcat Applications in a big number of nodes'
 depends 'windows', '~> 1.44.1'
 depends 'tomcat_manager'
 
